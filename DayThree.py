d16ef84442e0d1c0a3ee11a70b59956cafafddce
Jens Lordén <jenslorden@gmail.com>
2018-12-05 21:04:22 +0100

    Added day three part one solution and data.

---
+++ DayThree.py
@@ -0,0 +1,57 @@
+import InputReader
+
+######################
+# Day three part one #
+######################
+
+def ParseInput(input):
+    top_left = input.split()[2][0:-1]   # Remove the ':' at the end
+    height_width = input.split()[3]
+
+    n_left = int(top_left.split(',')[0])
+    n_top = int(top_left.split(',')[1])
+    n_width = int(height_width.split('x')[0])
+    n_height = int(height_width.split('x')[1])
+
+    return n_left, n_top, n_width, n_height
+
+def PrintFabric(fabric):
+    for column in fabric:
+        rowStr = ""
+        for row in column:
+            rowStr += str(row)
+        print(rowStr)
+
+def GetOverlaps(fabric, inputs):
+    overlaps = 0
+    for input in inputs:
+        n_left, n_top, n_width, n_height = ParseInput(input)
+        y = n_top
+        for h in range(n_height):
+            x = n_left
+            for w in range(n_width):
+                fabric[y][x] += 1
+                if fabric[y][x] == 2:
+                    overlaps += 1
+                x += 1
+            y += 1
+    return fabric, overlaps
+
+def CreateFabric(width, height):
+    return [[0 for x in range(width)] for y in range(height)]
+
+def PartOne():
+    inputs = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
+    fabric = CreateFabric(8, 8)
+    fabric, overlaps = GetOverlaps(fabric, inputs)
+    PrintFabric(fabric)
+    assert overlaps == 4, "{} should be 4"
+    
+    inputs = InputReader.ReadFileStr("DayThreeInput")
+    fabric = CreateFabric(1000, 1000)
+    fabric, overlaps = GetOverlaps(fabric, inputs)
+    print("Overlaps: {}".format(overlaps))
+
+
+if __name__ == "__main__":
+    PartOne()
